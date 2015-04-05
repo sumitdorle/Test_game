022100da21fab1ae2bc20820e9235f9e9f172b8c
sumitdorle <sumitdorle@gmail.com>
2015-04-05 03:02:33 +0000

    moving to pointer implemented

--- game.js
+++ game.js
@@ -1,43 +1,66 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 var tank1;
-var brick;
+var tank2;
+var bullet;
 var cursors;
 
+
 function preload() {
-    game.load.image('tank', 'tank.png', 120, 120);
-    //game.load.image('bricks', 'bricks.png', 120, 120);
+    game.load.image('tank', 'assets/tank.png', 120, 120);
+    game.load.image('tank2', 'assets/tank2.png', 120, 120);
+    game.load.image('bullet', 'assets/bullet.png', 120, 120);
 }
 
 function create() {
-    //game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     tank1 = game.add.sprite(0, 0, 'tank');
-   
-    // game.physics.arcade.enable(tank1);
-    // //tank1.body.gravity.y = 8;
-    // tank1.body.collideWorldBounds = true;
-    // cursors = game.input.keyboard.createCursorKeys();
-    // tank1.pivot.x = tank1.width * .5;
-    // tank1.pivot.y = tank1.height * .5;
+    tank1.inputEnabled = true;
+    bullet = game.add.sprite(0, 0, 'bullet');
+    game.physics.arcade.enable(tank1);
     
-    //Adding target
-    // game.physics.startSystem(Phaser.Physics.ARCADE);
-    // brick = game.add.sprite(game.world.centerX, 0, 'bricks');
-    // game.physics.arcade.enable(brick);
-    // game.add.tween(brick).to({ x: 400 }, 1000, Phaser.Easing.Linear.None, true, 0);
-   
+    tank2 = game.add.sprite(400, 0, 'tank2');
 
-    brick.body.collideWorldBounds = true;
+    //game.physics.arcade.enable(tank1);
+    tank1.body.collideWorldBounds = true;
+    cursors = game.input.keyboard.createCursorKeys();
+    tank1.pivot.x = tank1.width * .5;
+    tank1.pivot.y = tank1.height * .5;
+    
+    
+    //moving bullet to pointer
+    game.physics.arcade.enable(bullet);
+    game.camera.follow(bullet);
+    game.input.onDown.add(movebullet, this);
+    
+
 }
 
 function update() {
-
-
-
-
-
-
-
+    moveTank(tank1);
+    
+    if (bullet.body.collideWorldBounds){
+        bullet.kill(0);
+    }
 }
+
+// function removeBullet () {
+//     if (bullet.body.collideWorldBounds){
+//             this.bullet.kill();
+//     this.camera.follow();
+//     this.add.tween(this.camera).to( { x: 0 }, 1000, "Quint", true, 1000);
+//     }    
+// }
+
+
+
+
+function movebullet() {
+    game.camera.follow();
+    game.physics.arcade.moveToPointer(bullet, 400);
+}
+
+
+
 
 function moveTank(tank)
 {
@@ -57,6 +80,5 @@ function moveTank(tank)
     else if(cursors.right.isDown) {
       tank.body.velocity.x += 50;
       tank.scale.x =-1;
-        
     }
 }
