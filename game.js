1fa0b625ce58ac37c0795c4339e8bbe22f5e3a7b
sumitdorle <sumitdorle@gmail.com>
2015-04-05 05:11:15 +0000

    working on group to multifire a bomb

--- game.js
+++ game.js
@@ -2,8 +2,10 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 var tank1;
 var tank2;
 var bullet;
+var bullets;
 var cursors;
-
+var positionX;
+var positionY;
 
 function preload() {
     game.load.image('tank', 'assets/tank.png', 120, 120);
@@ -14,12 +16,34 @@ function preload() {
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
     tank1 = game.add.sprite(0, 0, 'tank');
-    tank1.inputEnabled = true;
-    bullet = game.add.sprite(0, 0, 'bullet');
     game.physics.arcade.enable(tank1);
+    tank1.inputEnabled = true;
+
+    //bullet = game.add.sprite(0, 0, 'bullet');
+    //game.physics.arcade.enable(bullet);
+    //bullet.anchor.setTo(tank1.anchor.x/2, tank1.anchor.y/2);
+    
+    
+//***************create bullet group**********************    
+    
+    //  Our ships bullets
+    bullets = game.add.group();
+    bullet = game.add.sprite(0, 0, 'bullet');
+
+    //  All 40 of them
+    bullets.createMultiple(40, 'bullet');
+    // bullets.setAll('anchor.x', 0.5);
+    // bullets.setAll('anchor.y', 0.5);
+//***********************************************************    
+    
+    
+    
+    
+    
     
     tank2 = game.add.sprite(400, 0, 'tank2');
-
+    game.physics.arcade.enable(tank2);
+    
     //game.physics.arcade.enable(tank1);
     tank1.body.collideWorldBounds = true;
     cursors = game.input.keyboard.createCursorKeys();
@@ -37,26 +61,46 @@ function create() {
 
 function update() {
     moveTank(tank1);
-    
+    this.physics.arcade.collide(bullet, tank2, killTanker, null, this);
+   
     if (bullet.body.collideWorldBounds){
         bullet.kill(0);
     }
+    
+
+    
+    
 }
 
-// function removeBullet () {
-//     if (bullet.body.collideWorldBounds){
-//             this.bullet.kill();
-//     this.camera.follow();
-//     this.add.tween(this.camera).to( { x: 0 }, 1000, "Quint", true, 1000);
-//     }    
-// }
+function killTanker(){
+    tank2.kill(0);
+    bullet.kill(0);
+}
 
+
+// function fireBullet () {
+
+//     if (game.time.now > bulletTime)
+//     {
+//         bullet = bullets.getFirstExists(false);
+
+//         if (bullet)
+//         {
+//             bullet.reset(sprite.body.x + 16, sprite.body.y + 16);
+//             bullet.lifespan = 2000;
+//             bullet.rotation = sprite.rotation;
+//             game.physics.arcade.velocityFromRotation(sprite.rotation, 400, bullet.body.velocity);
+//             bulletTime = game.time.now + 50;
+//         }
+//     }
+
+// }
 
 
 
 function movebullet() {
     game.camera.follow();
-    game.physics.arcade.moveToPointer(bullet, 400);
+    game.physics.arcade.moveToPointer(bullet, 600);
 }
 
 
